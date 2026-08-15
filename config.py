8557530defb7d68b62ab0ba12c54061215fb7354
Ankit Kumar <contact@deblomper.dev>
2026-08-16 01:31:52 +0530

    refactor: move ADMIN_IDS configuration to environment variables and remove admin.txt

--- config.py
+++ config.py
@@ -9,3 +9,6 @@ API_HASH = os.environ["API_HASH"]
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 GROUP = os.environ.get("GROUP", "nub_coder_s")
 CHANNEL = os.environ.get("CHANNEL", "nub_coders")
+ADMIN_IDS = [
+    int(x) for x in os.environ.get("ADMIN_IDS", "").replace(",", " ").split() if x.isdigit()
+]
